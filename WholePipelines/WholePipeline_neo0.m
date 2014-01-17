ab30e4791e958daa07e14e7e235bab1a402ca5d9
Davide Heller <davide.heller@imls.uzh.ch>
2014-01-17 15:44:03 +0100

    Final neo0 pipeline used to produce the final files used for the skeleton conversion

--- WholePipelines/WholePipeline_neo0.m
+++ WholePipelines/WholePipeline_neo0.m
@@ -1,7 +1,7 @@
 %% SETUP
 
 addpath('/Users/l48imac2/Documents/Userdata/Simon/Epitools/MatlabScripts')
-% make sure matlab has access to this java file!
+%make sure matlab has access to this java file!
 javaaddpath('/Users/l48imac2/Documents/Userdata/Simon/Epitools/OME_LOCI_TOOLS/loci_tools.jar')
 addpath('/Users/l48imac2/Documents/Userdata/Simon/Epitools/OME_LOCI_TOOLS')
 
@@ -9,14 +9,15 @@ addpath('/Users/l48imac2/Documents/Userdata/Simon/Epitools/OME_LOCI_TOOLS')
 
 %% READING ORIGINAL MICROSCOPY DATA
 
-DataDirec = '/Users/l48imac2/Documents/Userdata/Simon/decadGFP_103h_63XNE0_JHIII_20130912_84346 AM/2';
+DataDirec = '/Users/l48imac2/Documents/Userdata/Simon/decadGFP_103h_63XNE0_JHIII_20130912_84346 AM/0/Test1Output';
 
 % create directory where to store results of analysis
 AnaDirec = [DataDirec,'/Analysis'];
 mkdir(AnaDirec)
 
-
-Filemask = 'decadGFP_103h_63XNE0_JHIII_f';
+%Filemask = 'frame_085b_525e963c9e3a7_hrm.tif'; %same for 028 and 068, 089
+%because of bright spot correction
+Filemask = '.tif';
 
 lst = dir(DataDirec);  
 
@@ -39,19 +40,22 @@ end
 
 %% read first file
 
-FullDataFile = [DataDirec,'/',lst(4).name];
+FullDataFile = [DataDirec,'/',lst(first_index).name];
 Series = 1;
 res = ReadMicroscopyData(FullDataFile, Series);
 
-%% Projection
+%% Setup parameters for Projection
+fprintf('Started projection at %s',datestr(now));
 
 %Paramaters
 SmoothingRadius = 1.;           % how much smoothing to apply to original data (1-5)
-SurfSmoothness1 = 50;          % 1st surface fitting, surface stiffness ~100
+SurfSmoothness1 = 50;           % 1st surface fitting, surface stiffness ~100
 SurfSmoothness2 = 30;           % 2nd surface fitting, stiffness ~50
 ProjectionDepthThreshold = 1.2; % how much up/down to gather data from surface
-%
+
+%Number of time points to be analyzed
 NT = res.NT;
+%NT = 3;
 
 RegIm = zeros(res.NY,res.NX,NT);
 Surfaces = zeros(res.NY,res.NX,NT);
@@ -61,18 +65,21 @@ InspectResults = false;         % show fit or not
 
 Series = 1;
 
-matlabpool 2
+%matlabpool 2
 d = 0;
 
-for i =1:length(lst) %first_index 
+% For loop for all files in the folder (lst) and second parfor for all timepoints
+
+for i =1:length(lst)
     if isempty(strfind(lst(i).name,Filemask)); continue; end;
     FullDataFile = [DataDirec,'/',lst(i).name];
-    res = ReadMicroscopyData(FullDataFile, Series); 
+    res = ReadMicroscopyData(FullDataFile, Series);
     res.images = squeeze(res.images); % get rid of empty 
     fprintf('Working on %s\n', lst(i).name);
    
-    
-    parfor f = 1:res.NT 
+    %information seems to not be transmitted correctly 10 time points
+    %appear to be presente while there are only 3, CHECK [ ] 
+    for f = 1:res.NT 
         ImStack = res.images(:,:,:,f);
         [im,Surf] = createProjection(ImStack,SmoothingRadius,ProjectionDepthThreshold,SurfSmoothness1,SurfSmoothness2,InspectResults);
         ProjIm(:,:,f+d) = im;
@@ -85,7 +92,10 @@ end
 % inspect results
 StackView(ProjIm);
 
-matlabpool close
+%matlabpool close
+
+fprintf('Finished projection at %s',datestr(now));
+
 %% REGISTRATION
 matlabpool 2
 
@@ -96,11 +106,38 @@ StackView(RegIm);
 
 %saving results
 save([AnaDirec,'/RegIm'],'RegIm');
+
 matlabpool close
 
 
+%% CLAHE - Contrast-Limited Adaptive Histogram Equalization
 
-%% SEGMENTATION CURRENTLY SET FOR 10 FRAMES ONLY, on new iMac with 4C ~10min/frame
+%help: http://www.mathworks.ch/ch/help/images/ref/adapthisteq.html
+
+fprintf('Started CLAHE at %s',datestr(now));
+
+%pre-allocate output
+RegIm_clahe = zeros(size(RegIm,1), size(RegIm,2), size(RegIm,3), 'double');
+
+%needs prior conversion for method
+RegIm_uint16 = zeros(size(RegIm,2), size(RegIm,2), 'uint16');
+
+%pre-alloacation for speed
+RegIm_clahe_uint16 = zeros(size(RegIm,2), size(RegIm,2), 'uint16');
+
+for i=1:size(RegIm,3)
+    RegIm_uint16 = uint16(RegIm(:,:,i));
+    RegIm_clahe_uint16 = adapthisteq(RegIm_uint16,'NumTiles',[70 70],'ClipLimit',0.02);
+    RegIm_clahe(:,:,i) = double(RegIm_clahe_uint16); 
+end
+
+
+StackView(RegIm_clahe);
+
+fprintf('Stopped CLAHE at %s',datestr(now));
+
+%% SEGMENTATION modified for clahe!
+fprintf('Started SEGMENTATION at %s',datestr(now));
 
 matlabpool 4
 
@@ -118,30 +155,38 @@ params.sigma3=2;
 params.LargeCellSizeThres = 3000;
 params.MergeCriteria = 0.35;
 %final joining
-params.IBoundMax = 80;          % 30 for YM data
+params.IBoundMax = 30;          % 30 for YM data
 
 % show steps
 params.show = false;
-%Parallel true very slow, check [ ]
 params.Parallel  = true;
 
-[ILabels , CLabels , ColIms] = SegmentStack(RegIm(:,:,1:10), params);
+[ILabels , CLabels , ColIms] = SegmentStack(RegIm_clahe, params);
 
-StackView(ColIms)
+fprintf('Stopped SEGMENTATION at %s',datestr(now));
 
-save([AnaDirec,'/SegResults'], 'RegIm', 'ILabels', 'CLabels' ,'ColIms','params')
+%StackView(ColIms)
 
-% save([AnaDirec,'/SegResults'], 'RegIm', 'ILabels', 'CLabels' ,'ColIms','params','NX','NY','NT')
+%% If successful save RegIm_clahe as Regim and save 
+
+RegIm = RegIm_clahe;
+
+NX = size(RegIm,1);
+NY = size(RegIm,2);
+NT = size(RegIm,3);
+
+save([AnaDirec,'/SegResults'], 'RegIm', 'ILabels', 'CLabels' ,'ColIms','params','NX','NY','NT')
 
 matlabpool close
 
-%% Add elipse crop to avoid tracking false structures.
+
+%% Add elipse crop to avoid tracking false structures. (not needed for deconvolved images)
 
 BW = GetEllipse(RegIm(:,:,1));
 
 CLabelsEll = zeros(size(RegIm));
 
-for f = 1 : 10
+for f = 1 : size(RegIm,3)
     I1 = CLabels(:,:,f);
     I1(BW < 1) = 0;
     Ls = unique(I1);
@@ -152,13 +197,27 @@ for f = 1 : 10
 end
 StackView(CLabelsEll)
 
+
+%% Save ellipse selection changes
+
+save([AnaDirec,'/CLabelsBKP'],'CLabels');
+
 CLabels = CLabelsEll;
 
-save([AnaDirec,'/SegResults'], 'RegIm', 'ILabels', 'CLabels' ,'ColIms','params')
+save([AnaDirec,'/SegResults'], 'RegIm', 'ILabels', 'CLabels' ,'ColIms','params','-v7.3');
 
-%% TRACKING
+
+%% Open matlabpool for Tracking
+
+
+
+%% TRACKING 
 
 load([AnaDirec,'/SegResults']);
+
+NX = size(RegIm,1);
+NY = size(RegIm,2);
+NT = size(RegIm,3);
 
 params.TrackingRadius = 15;
 output = 'ILabelsCorrected';
@@ -166,7 +225,9 @@ output = 'ILabelsCorrected';
 try
     P = load([AnaDirec,'/SegResultsCorrected']);
     disp('Using previous segmentation as baseline');
-    fig = TrackingGUI(RegIm,P.ILabels,P.CLabels,P.ColIms,output,params);
+    %changed the Tracking framework such that it can read previous OK
+    %trajectories
+    fig = TrackingGUIwOldOK(RegIm,P.ILabels,P.CLabels,P.ColIms,output,params,IL.oktrajs);
     
     CLabels = P.CLabels;       % using previous segmemtation
     ColIms = P.ColIms;
@@ -178,13 +239,65 @@ end
 % wait for corrections to finish (ie after saving using 's')
 uiwait(fig);
 
-% now resegmenting the frames which need it!
+%TrackingGUI takes care of saving a copy of the changes in the current
+%matlab working directory [BACKUP if needed!]
+
+%% now resegmenting the frames which need it!
+
+%given the reduced amuont of of frames parallelization is manually set
+%as otherwise always all frames would be resegmented. Judge according
+%to the case if to activate or not
+
+params.Parallel = true; 
+
+if(params.Parallel)
+    matlabpool 4
+end
+
+fprintf('Started SEGMENTATION at %s',datestr(now));
+
 IL = load(output);
 [ILabels , CLabels , ColIms] = SegmentStack( RegIm , params , IL.ILabels ,CLabels, ColIms, IL.FramesToRegrow );
-    
-save([AnaDirec,'/SegResultsCorrected'], 'RegIm','ILabels', 'CLabels' ,'ColIms','params','NX','NY','NT');
+
+fprintf('Stopped SEGMENTATION at %s',datestr(now));
+
+if(params.Parallel)
+    matlabpool close
+end
+
+% added version option to save ColIms as well /skipped otherwise, added 
+save([AnaDirec,'/SegResultsCorrected'], 'RegIm','ILabels', 'CLabels' ,'ColIms','params','NX','NY','NT','IL','-v7.3' );
+
+%% TESTING RESULTS SECTION / enhance for multiple correction procedure
+
+BackupDir = [AnaDirec,'/BKP'];
+
+%load([BackupDir,'/ColImsBKP']);
+%load([BackupDir,'/IlabelsCorrected']);
+
+%% reload previous results first / not available as ColIms was never saved before tracking (v7.3 MOD)
+
+fig = TrackingGUI(RegIm,IL.ILabels,CLabels,ColIms,output,params);
+uiwait(fig);
+
+%% new results, 
+
+fig = TrackingGUI(RegIm,P.ILabels,P.CLabels,ColIms,output,params);
+uiwait(fig);
+
+%% old results with old OKs
+
+fig = TrackingGUIwOldOK(RegIm,IL.ILabels,CLabels,ColIms,output,params,IL.oktrajs);
+uiwait(fig);
+
+
+%% new results with old OKs
+
+fig = TrackingGUIwOldOK(RegIm,ILabels,CLabels,ColIms,output,params,IL.oktrajs);
+uiwait(fig);
+
+
+%% Close workers
 
 StackView(ColIms);
 StackView(CLabels);
-
-
