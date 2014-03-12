406f043adde0c46dd7bbbdf44fba88ee56624721
Davide Heller <davide.heller@imls.uzh.ch>
2014-03-12 14:46:20 +0100

    RepeatTrackingGUI: added verification of sequence dimension correspondence. Furthermore refactored to improve readability

--- HelperScripts/RepeatTrackingGUI.m
+++ HelperScripts/RepeatTrackingGUI.m
@@ -5,64 +5,63 @@
 % email:    davide.heller@imls.uzh.ch
 % date:     2014-02-13
 
-% help: proceed through the selections by pressing cmd + enter
+% help: Proceed through the sections by pressing cmd + enter, 
+%       the last section can be executed repeatedly without risking to 
+%       overwrite the previous results.
 
-%% SETUP
+%% Epitools setup
 
-%retrieve needed files from current script location
-%requires Matlab 7+
+%retrieve needed files from current script location requires Matlab 7+
+%assuming this file is executed from repository location!
 current_script_path = matlab.desktop.editor.getActive().Filename;
-fprintf('Script location:%s\n',current_script_path);
-
 [file_path,file_name,file_ext] = fileparts(current_script_path);
 cd(file_path)
 
+% set epitool script location
 addpath([fileparts(file_path),'/MatlabScripts'])
-%make sure matlab has access to this java file!
 javaaddpath([fileparts(file_path),'/OME_LOCI_TOOLS/loci_tools.jar'])
 addpath([fileparts(file_path),'/OME_LOCI_TOOLS']) 
 
-%% Repeat tracking correction with manually specified file 
+%% Load Image and Segmentation 
 
 [filename, pathname] = uigetfile('.mat','Select SegResults.mat');
+disp(['Retrieving results from:',pathname])
+disp('Starting to load SegResults...');
+load([pathname,'SegResults']);
+disp('...Finished loading SegResults');
 
-AnaDirec = pathname;
-
-disp(['Retrieving results from:',AnaDirec])
-
-cd(AnaDirec)
-
-load([AnaDirec,'/SegResults']);
-
+%Save original sequence dimensions
 NX = size(RegIm,1);
 NY = size(RegIm,2);
 NT = size(RegIm,3);
 
+%Optional parameter for the TrackingGUI
 params.TrackingRadius = 15;
 
-%% If this is correct (Check Command Window) proceed to tracking correction
+%Change Directory to save the tracking corrections
+cd(pathname)
+
+%% Tracking correction
 
 %save new tracking results with new timestamp
 %e.g. ILabelsCorrected_20140213T144649
 output = ['ILabelsCorrected_',datestr(now,30)];
 
+%retrieve previous tracking file
 [filename, pathname] = uigetfile('.mat','Select last tracking file');
+IL = load([pathname,filename]);
+disp(['Current tracking file: ',filename]);
 
-%Removed comments in order to see whether licensing
-%error is the problem. Only solution seems to repeatedly
-%try to obtain the license. (image processing toolbox)
-%try
-    %open last tracking file
-    IL = load([pathname,filename]);
-    fig = TrackingGUIwOldOK(RegIm,IL.ILabels,CLabels,ColIms,output,params,IL.oktrajs,IL.FramesToRegrow);
-%catch
-%    disp('no previous segmentation')
-%end
+%patch to avoid the increase in x,y dimensions
+IL.ILabels = IL.ILabels(1:NX,1:NY,:);
+
+%open the tracking gui
+fig = TrackingGUIwOldOK(RegIm,IL.ILabels,CLabels,ColIms,output,params,IL.oktrajs,IL.FramesToRegrow);
 
 % wait for corrections to finish (ie after saving using 's')
 uiwait(fig);
 
-%% Check if the new size exeeds the previous one!
+%% Final Check if the new size exeeds the previous one!
 load(output);
 
 %% if yes reSize and reSave!
