9a4bb9bd04eca6cfff94b9333e90f979ab408187
Alexander Tournier <alexandertournier@gmail.com>
2014-05-23 16:52:42 +0100

    TEST: file comparison gives more info

--- EpitoolComponents/CompareFiles.m
+++ EpitoolComponents/CompareFiles.m
@@ -5,6 +5,7 @@ C2 = load(f2);
 
 if ~isequal(C1,C2)
     errordlg('Files generated are not the same!','File comparison error');
+    fprintf('file comparison FAILED HERE: %s vs %s\n', f1,f2);
 else
     fprintf('file comparison passed\n');
 end
