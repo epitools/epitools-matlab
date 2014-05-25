0d1e4f166b6eb14f031a925065008eb713164c8a
Davide Heller <davide.heller@imls.uzh.ch>
2014-05-25 21:51:47 +0200

    Refactoring: Added headers to Registration files and marks for further commenting

--- MatlabScripts/RegisterStack.m
+++ MatlabScripts/RegisterStack.m
@@ -1,28 +1,38 @@
 function RegIm = RegisterStack(ProjIm)
+% RegisterStack Register a time series to reduce sample movement 
+%
+% IN: 
+%   ProjIm - Projected Image time series                                   (substitute with ImStack?)
+% 
+% OUT: 
+%   RegIm - 
+%
+% Author:
+% Copyright:
 
-%% parameters (quite robust so far!)
+%% parameters (quite robust so far!)                                       
 
-Ang = [-5:1:5];
+Ang = [-5:1:5];                                                            % what are these parameters?
 X = [-150:10:150];
 Y = [-150:10:150];
 
 FAng = -1.5:.1:1.5;
 FX = [-15:1:15];
 FY = [-15:1:15];
-%%
+%%                                                                         % add section title/comment
 
 NImages = size(ProjIm,3);
 s = size(ProjIm);
 ImSize = s(1:2);
 
-%%
+%%                                                                         % add section title/comment
 f1=fspecial( 'gaussian', ImSize, .1);
 se = strel('disk',10);
 
 fprintf('Building coarse view of stack ...')
 
 
-Coarse = zeros([ImSize,NImages]);
+Coarse = zeros([ImSize,NImages]);                                          % explain Coarse matrix
 parfor i = 1:NImages
     T = ProjIm(:,:,i);
     T(T<.4) = 0;
@@ -44,21 +54,21 @@ parfor i = 1:NImages
 end
 fprintf('done\n');
 
-%%
+%%                                                                         % add section title/comment
 
 fprintf('Registering projected images ...\n');
 
 RegRes = {};
 RegRes2 = {};
 
-parfor i = 2:NImages
+parfor i = 2:NImages                                                       % why 2 iterations?
     fprintf('%i/%i\n', i,NImages);
-    RegRes = Register(Coarse(:,:,i),Coarse(:,:,i-1),Ang,X,Y,'opt');
+    RegRes = Register(Coarse(:,:,i),Coarse(:,:,i-1),Ang,X,Y,'opt');        
     %finer, 2nd go!
     RegRes2{i} = Register(Coarse2(:,:,i),Coarse2(:,:,i-1),FAng + RegRes.Ang ,FX +RegRes.x ,FY + RegRes.y ,'opt');
 end
 
-%%
+%%                                                                         % add section title/comment
 
 RegRes2{1}.CumX = 0;
 RegRes2{1}.CumY = 0;
