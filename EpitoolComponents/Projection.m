0ed25be3b46095c977406235a3110ad7ceb129a9
Alexander Tournier <alexandertournier@gmail.com>
2014-05-26 15:31:53 +0100

    PROJ: typecasting and renaming vars

--- EpitoolComponents/Projection.m
+++ EpitoolComponents/Projection.m
@@ -13,12 +13,12 @@ function Projection(DataSpecificsPath,params)
 progressbar('Projecting images...');
 
 load(DataSpecificsPath);
-res = ReadMicroscopyData(FullDataFile, Series);
+Data = ReadMicroscopyData(FullDataFile, Series);
 % todo: need to convert to int8 in a rational way here!
 % can use 95 or 99% quantile of the data and then scale
 
-Surfaces = zeros(res.NY,res.NX,res.NT);
-ProjIm = zeros(res.NY,res.NX,res.NT);
+Surfaces = zeros(Data.NY,Data.NX,Data.NT,'uint8');
+ProjIm = zeros(Data.NY,Data.NX,Data.NT, Data.PixelType);
 
 d = 0;
 
@@ -27,27 +27,27 @@ fprintf('Started projection at %s',datestr(now));
 for i =1:length(lst)
     if isempty(strfind(lst(i).name,Filemask)); continue; end;
     FullDataFile = [DataDirec,'/',lst(i).name];
-    res = ReadMicroscopyData(FullDataFile, Series);
-    res.images = squeeze(res.images); % get rid of empty 
+    Data = ReadMicroscopyData(FullDataFile, Series);
+    Data.images = squeeze(Data.images); % get rid of empty 
     fprintf('Working on %s\n', lst(i).name);
    
     %information seems to not be transmitted correctly 10 time points
     %appear to be presente while there are only 3, CHECK [ ] 
-    for f = 1:res.NT 
-        ImStack = res.images(:,:,:,f);
+    for f = 1:Data.NT 
+        ImStack = Data.images(:,:,:,f);
         [im,Surf] = createProjection(ImStack,params.SmoothingRadius,params.ProjectionDepthThreshold,params.SurfSmoothness1,params.SurfSmoothness2,params.InspectResults);
         ProjIm(:,:,f+d) = im;
         Surfaces(:,:,f+d) = Surf;
-        progressbar(((i-1)*res.NT+f)/length(lst)/res.NT);
+        progressbar(((i-1)*Data.NT+f)/length(lst)/Data.NT);
     end
-    d=d+res.NT;
+    d=d+Data.NT;
 end
 
 save([AnaDirec,'/ProjIm'],'ProjIm')
 save([AnaDirec,'/Surfaces'],'Surfaces')
 
 progressbar(1);
-fprintf('Finished projection at %s',datestr(now));
+fprintf('Finished projection at %s\n',datestr(now));
 
 if params.InspectResults
     StackView(ProjIm);
