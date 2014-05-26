76426ff351a5b440abe8c608cce1b84820a8054a
Alexander Tournier <alexandertournier@gmail.com>
2014-05-26 10:06:01 +0100

    PROJ: added idea for uint8 conversion

--- MatlabScripts/RegisterStack.m
+++ MatlabScripts/RegisterStack.m
@@ -1,14 +1,18 @@
 function RegIm = RegisterStack(ProjIm)
+% Registration of the images in the stacks
+% the process uses 2 steps: one coarse and one fine
+%
 
-%% parameters (quite robust so far!)
+% Parameters for the coarse registration step
+Ang = [-5:1:5];             % angle differences to explore
+X = [-150:10:150];          % Delta X to explore
+Y = [-150:10:150];          % Delta Y to explore
 
-Ang = [-5:1:5];
-X = [-150:10:150];
-Y = [-150:10:150];
-
+% Parameters for the fine registration step (finer steps)
 FAng = -1.5:.1:1.5;
 FX = [-15:1:15];
 FY = [-15:1:15];
+
 %%
 
 NImages = size(ProjIm,3);
@@ -21,9 +25,9 @@ se = strel('disk',10);
 
 fprintf('Building coarse view of stack ...')
 
-
+% building a coarse view of the stack
 Coarse = zeros([ImSize,NImages]);
-parfor i = 1:NImages
+for i = 1:NImages           
     T = ProjIm(:,:,i);
     T(T<.4) = 0;
     closeBW = imclose(T,se);
@@ -32,6 +36,7 @@ parfor i = 1:NImages
     Coarse(:,:,i ) = Im2;
 end
 
+% building a finer view of the stack
 se = strel('disk',5);
 Coarse2 = zeros([ImSize,NImages]);
 parfor i = 1:NImages
