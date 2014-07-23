6052e79b4ecb9582be87b6695982b52e461efa03
Davide Heller <davide.heller@imls.uzh.ch>
2014-07-23 09:59:58 +0200

    PolyCrop: Added error try/catch in case the user closes the polygonal mask creation before completion. Added the polygonal mask as output for later use

--- EpitoolComponents/PolygonCrop.m
+++ EpitoolComponents/PolygonCrop.m
@@ -1,37 +1,40 @@
-function cropped_CellLabelIm = PolygonCrop( imStack, CellLabelIm )
+function [polygonal_mask, cropped_CellLabelIm] = PolygonCrop( imStack, CellLabelIm )
 %POLYGONCROP provides an interactive polygon crop to reduce the segmented
 %area of the image in case of extended wrong signal. Aim is to preserve the
 %cell geometry and include all cells touched by the selection.
+%source:
+%- original code snipped by Alexandre Tournier
+%- http://www.mathworks.com/matlabcentral/answers/99870-how-can-i-extract-a-portion-of-an-image-specified-by-mask-generated-from-a-roi-in-image-processing-t
 
    
     %% Creating the mask
     %  Show the first time point and wait for the user to insert the polygon
     %mask. This can be done by clicking on the image and confirming at the
     %end with a right click on the mask -> create Mask
-    fig = figure('name','Draw polygon and confirm by rightclick->create mask');
-    imshow(imStack(:,:,1),[]);
-    polygonal_mask = roipoly;
-    close(fig); 
-    
+    try
+        fig = figure('name','Draw polygon and confirm by rightclick->create mask');
+        imshow(imStack(:,:,1),[]);
+        polygonal_mask = roipoly;
+        close(fig); 
+    catch
+        %user closed the figure without completing the procedure
+        polygonal_mask = 0;
+        cropped_CellLabelIm = 0;
+        return
+    end
     %% Produce the cropped image stack
     cropped_CellLabelIm = zeros(size(CellLabelIm));
     frame_no = size(CellLabelIm,3);
 
     %apply the mask frame by frame
     for f = 1 : frame_no
-        frame = CellLabelIm(:,:,f);
         
-        %identify the outer region of the mask
-        outer_region = (polygonal_mask < 1);
         %set everything in the outer region to 0
-        frame(outer_region) = 0;
-        
-        %alternative: 
-        %setZero = (@(x)0);
-        %filtered_frame = roifilt2(frame,outer_region,setZero);
+        ROI = CellLabelIm(:,:,f);
+        ROI(polygonal_mask == 0) = 0;
         
         %find which cells labels have not been set to 0 
-        cell_labels_to_keep = unique(frame);
+        cell_labels_to_keep = unique(ROI);
 
         %apply the selection of Id's
         cropped_frame = CellLabelIm(:,:,f);
