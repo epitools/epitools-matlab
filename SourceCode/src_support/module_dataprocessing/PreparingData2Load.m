99521f54b2d34b879f0cbed543b181ba2d228ee2
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2014-08-07 22:53:05 +0200

    PROC: Committing the new function @PreparingData2Load to the feature branch.

---
+++ SourceCode/src_support/module_dataprocessing/PreparingData2Load.m
@@ -0,0 +1,9 @@
+function [ output_args ] = PreparingData2Load( input_args )
+%PREPARINGDATA2LOAD This function will prepare your data to be loaded in
+%Epitools. This allows the programm to load only the files you previously
+%set to be sent to further analysis steps.
+
+
+
+end
+
