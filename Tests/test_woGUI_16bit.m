731dc55c62f12686364b2b363cf0d708fda4efc4
Davide Heller <davide.heller@imls.uzh.ch>
2014-08-08 11:21:32 +0200

    TEST: added contrast enhancement to tests and changed variable names of mdname to strModuleName for clarity.

--- Tests/test_woGUI_16bit.m
+++ Tests/test_woGUI_16bit.m
@@ -20,19 +20,19 @@ ds.platform_units = 1;
  
 %% Surface Projection
 
-mdname = 'Projection';
-ds.CreateModule(mdname);
+strModuleName = 'Projection';
+ds.CreateModule(strModuleName);
 
 % how much smoothing to apply to original data - default 1 [1-5]
-ds.AddSetting(mdname, 'SmoothingRadius', 1.0); 
+ds.AddSetting(strModuleName, 'SmoothingRadius', 1.0); 
 % 1st surface fitting, surface stiffness - default 100 [50 - 200]
-ds.AddSetting(mdname, 'SurfSmoothness1', 100);        
+ds.AddSetting(strModuleName, 'SurfSmoothness1', 100);        
 % 2nd surface fitting, stiffness          - default 30 [10 - 50]
-ds.AddSetting(mdname, 'SurfSmoothness2', 35);
+ds.AddSetting(strModuleName, 'SurfSmoothness2', 35);
 % how much up/down to gather data from surface - default 1.2 [1 - 3]
-ds.AddSetting(mdname, 'ProjectionDepthThreshold', 1.2);
+ds.AddSetting(strModuleName, 'ProjectionDepthThreshold', 1.2);
 % show fit or not
-ds.AddSetting(mdname, 'InspectResults', false);
+ds.AddSetting(strModuleName, 'InspectResults', false);
 
 Projection(ds);
 
@@ -43,13 +43,23 @@ CompareFiles('Data/Analysis/ProjIm' , 'Data/Benchmark/ProjIm');
 CompareFiles('Data/Analysis/Surfaces' , 'Data/Benchmark/Surfaces');
 
 %% Time Series Registration
-mdname = 'Stack_Registration';
-ds.CreateModule(mdname);
+strModuleName = 'Stack_Registration';
+ds.CreateModule(strModuleName);
 
-ds.AddSetting(mdname, 'SkipFirstRegStep', true); 
-ds.AddSetting(mdname, 'useStackReg', false); 
+ds.AddSetting(strModuleName, 'SkipFirstRegStep', true); 
+ds.AddSetting(strModuleName, 'useStackReg', false); 
 
 Registration(ds); 
+
+CheckInputType(ds, 'RegIm');
+
+%% Apply CLAHE
+strModuleName = 'Contrast_Enhancement';
+ds.CreateModule(strModuleName);
+
+ds.AddSetting(strModuleName, 'enhancement_limit', 0.02);
+
+ImproveContrast(ds);
 
 CheckInputType(ds, 'RegIm');
 
@@ -57,29 +67,29 @@ CheckInputType(ds, 'RegIm');
 CompareFiles('Data/Analysis/RegIm' , 'Data/Benchmark/RegIm');
 
 %% Segmentation parameters:
-mdname = 'Segmentation';
-ds.CreateModule(mdname);
+strModuleName = 'Segmentation';
+ds.CreateModule(strModuleName);
 
 % area of cell in pixels
-ds.AddSetting(mdname, 'mincellsize', 25); 
+ds.AddSetting(strModuleName, 'mincellsize', 25); 
 % smoothing to be applied, needed to get rid of as much noise as poss 
 % without loosing the actual features we are looking for)
-ds.AddSetting(mdname, 'sigma1', 1);         
-ds.AddSetting(mdname, 'threshold', 25);               
+ds.AddSetting(strModuleName, 'sigma1', 1);         
+ds.AddSetting(strModuleName, 'threshold', 25);               
 
 % Grow cells
-ds.AddSetting(mdname, 'sigma3', 2);
-ds.AddSetting(mdname, 'LargeCellSizeThres', 3000);
+ds.AddSetting(strModuleName, 'sigma3', 2);
+ds.AddSetting(strModuleName, 'LargeCellSizeThres', 3000);
 % default 0.35 .. have to play with it
-ds.AddSetting(mdname, 'MergeCriteria', 0.35);   
+ds.AddSetting(strModuleName, 'MergeCriteria', 0.35);   
 
 % Final joining ,  30 for YM data
-ds.AddSetting(mdname, 'IBoundMax', 30); 
+ds.AddSetting(strModuleName, 'IBoundMax', 30); 
 
 % Performance Options (show=show_steps)
-ds.AddSetting(mdname, 'debug', false);
-ds.AddSetting(mdname, 'Parallel', false);
-ds.AddSetting(mdname, 'SingleFrame', false);
+ds.AddSetting(strModuleName, 'debug', false);
+ds.AddSetting(strModuleName, 'Parallel', false);
+ds.AddSetting(strModuleName, 'SingleFrame', false);
 
 
 Segmentation(ds);
