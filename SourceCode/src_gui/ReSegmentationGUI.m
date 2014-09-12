6cb80b3bbcad2937ca27a76b34bf250fc73096b7
Davide Heller <davide.heller@imls.uzh.ch>
2014-09-12 10:44:24 +0200

    ECS-56 Fixed wrong static link in ReSegmentation and added relative link

--- SourceCode/src_gui/ReSegmentationGUI.m
+++ SourceCode/src_gui/ReSegmentationGUI.m
@@ -82,7 +82,7 @@ function pushbutton1_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-web('http://imls-bg-arthemis.uzh.ch/epitools-wiki/site/Analysis%20Modules/segmentation/');
+web('http://imls-bg-arthemis.uzh.ch/epitools/?url=Analysis%20Modules/03_segmentation/');
 
 % --- Executes on button press in run_resegmentation.
 function run_resegmentation_Callback(hObject, eventdata, handles)
