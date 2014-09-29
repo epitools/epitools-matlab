4a19a4c9d49e2a68a3495913ba31565ffa0162ea
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2014-09-29 17:53:21 +0200

    ECS-114 #ADD: Added disclaimer when executing the software. User has to agree with NDA.

---
+++ SourceCode/src_framework/module_disclaimer/disclaimerGUI.m
@@ -0,0 +1,142 @@
+function varargout = disclaimerGUI(varargin)
+% DISCLAIMERGUI MATLAB code for disclaimerGUI.fig
+%      DISCLAIMERGUI by itself, creates a new DISCLAIMERGUI or raises the
+%      existing singleton*.
+%
+%      H = DISCLAIMERGUI returns the handle to a new DISCLAIMERGUI or the handle to
+%      the existing singleton*.
+%
+%      DISCLAIMERGUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in DISCLAIMERGUI.M with the given input arguments.
+%
+%      DISCLAIMERGUI('Property','Value',...) creates a new DISCLAIMERGUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before disclaimerGUI_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to disclaimerGUI_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help disclaimerGUI
+
+% Last Modified by GUIDE v2.5 29-Sep-2014 08:40:39
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @disclaimerGUI_OpeningFcn, ...
+                   'gui_OutputFcn',  @disclaimerGUI_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+% --- Executes just before disclaimerGUI is made visible.
+function disclaimerGUI_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to disclaimerGUI (see VARARGIN)
+
+% Choose default command line output for disclaimerGUI
+handles.output = 'Yes';
+
+% Update handles structure
+guidata(hObject, handles);
+
+% Make the GUI modal
+set(handles.figure1,'WindowStyle','modal')
+movegui(hObject,'center');
+% UIWAIT makes disclaimerGUI wait for user response (see UIRESUME)
+uiwait(handles.figure1);
+
+% --- Outputs from this function are returned to the command line.
+function varargout = disclaimerGUI_OutputFcn(hObject, eventdata, handles)
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+% The figure can be deleted now
+delete(handles.figure1);
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+handles.output = get(hObject,'String');
+
+% Update handles structure
+guidata(hObject, handles);
+
+% Use UIRESUME instead of delete because the OutputFcn needs
+% to get the updated handles structure.
+uiresume(handles.figure1);
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+handles.output = get(hObject,'String');
+
+% Update handles structure
+guidata(hObject, handles);
+
+% Use UIRESUME instead of delete because the OutputFcn needs
+% to get the updated handles structure.
+uiresume(handles.figure1);
+
+% --- Executes when user attempts to close figure1.
+function figure1_CloseRequestFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+if isequal(get(hObject, 'waitstatus'), 'waiting')
+    % The GUI is still in UIWAIT, us UIRESUME
+    uiresume(hObject);
+else
+    % The GUI is no longer waiting, just close it
+    delete(hObject);
+end
+
+% --- Executes on key press over figure1 with no controls selected.
+function figure1_KeyPressFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Check for "enter" or "escape"
+if isequal(get(hObject,'CurrentKey'),'escape')
+    % User said no by hitting escape
+    handles.output = 'No';
+    
+    % Update handles structure
+    guidata(hObject, handles);
+    
+    uiresume(handles.figure1);
+end    
+    
+if isequal(get(hObject,'CurrentKey'),'return')
+    uiresume(handles.figure1);
+end    
