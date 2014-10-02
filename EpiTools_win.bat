527273b7f3f4ca80dca815d5e2046b11605e7910
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2014-10-02 14:42:44 +0200

    HOTFIX: Added launcher for win systems.

--- EpiTools_win.bat
+++ EpiTools_win.bat
@@ -1,6 +1,3 @@
 @echo off
-
 SET scriptpath=%~dp0
-SET matlabpath="C:\Program Files\MATLAB\R2014a\bin\matlab.exe"
-
-%matlabpath% -nodesktop -nosplash -r "cd('%scriptpath%\src');EpiTools;"
+matlab -nodesktop -nosplash -r "cd('%scriptpath%\src');EpiTools;"
