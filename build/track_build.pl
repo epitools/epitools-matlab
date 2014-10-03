aa410f2b169f787abf752d110a439024977ed534
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2014-10-03 16:56:27 +0200

    FIX: Fix arg in script track_build

--- build/track_build.pl
+++ build/track_build.pl
@@ -75,7 +75,7 @@ if (exists($ARGV_HASH->{branch}))
         }
 
         # in case the branch where commit has happened is exactly "development"
-        case "development"
+        case "develop"
         {            
             # if this build is triggered only by a commit into development branch,
             # then increase release value of 0.1 each time
@@ -117,7 +117,7 @@ if (exists($inputfile->{date_version})){ $date_build = $inputfile->{date_build};
 my $head_build = strftime "%b", localtime;
 my $head_build_date = strftime "%a %d %b %Y %H:%M:%S (UTC %z)", localtime;
 $build =  $head_build .'-0'. $ARGV_HASH->{build_uid};
-$date_build = $head_build_date .' due to commit uid ' . $ARGV_HASH->{commit_uid};
+$date_build = $head_build_date .' with build key ' . $ARGV_HASH->{build_key};
 
 $inputfile->{build} = $build;
 $inputfile->{date_build} = $date_build;
