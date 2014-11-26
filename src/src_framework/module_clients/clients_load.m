31d610ff8c6565a89fc6dc54332c49e14edb1397
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2014-11-26 16:32:23 +0100

    ECS-44 #PREP: Polishing client loader function.

--- src/src_framework/module_clients/clients_load.m
+++ src/src_framework/module_clients/clients_load.m
@@ -9,20 +9,16 @@ function inst_client  = clients_load
 inst_client = clientd();
 % Explode src_analysis folder
 contents = dir('src_analysis');
-
 for i=1:numel(contents)
-    
     % Avoid considering file in the current directory
     if (contents(i).isdir ~= 1);continue;end
     if (strfind(contents(i).name,'module_'))
-        
         % Check if exist a file named> header.xml in each module_ prefixed
         % folders contained in src_analysis.
         if(exist(['src_analysis/',contents(i).name,'/header.xml'],'file'))
-            
             filename = ['src_analysis/',contents(i).name,'/header.xml'];
+            %inst_client = inst_client.addClient(filename, 'active');
             inst_client = inst_client.addClient(filename, 'active');
-            
         end
     end
 end
