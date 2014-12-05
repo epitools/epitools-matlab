0e2bb979281faf27003533e36cec6ab0630c0dc9
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2014-12-05 15:43:49 +0100

    ECS-122 #TEST: First attempt not completed to integrate legacy functions with new server-client approach. CLAHE module might not work with this commit.

--- src/src_analysis/module_clahe/ImproveContrast.m
+++ src/src_analysis/module_clahe/ImproveContrast.m
@@ -1,15 +1,52 @@
-function ImproveContrast(stgObj)
-%ImproveContrast Improve image contrast by applying CLAHE
-%   CLAHE - Contrast-Limited Adaptive Histogram Equalization
-%   DataSpecificsPath - Path Data to analyze (See InspectData function)
+function [status,argout] = ImproveContrast(input_args,varargin)
+%ImproveContrast Improve image contrast by applying CLAHE enhancement method
+% ------------------------------------------------------------------------------
+% PREAMBLE
+%
+% CLAHE operates on small regions in the image, called tiles, rather than the entire image. 
+% Each tile's contrast is enhanced, so that the histogram of the output region approximately 
+% matches the histogram specified by the 'Distribution' parameter. The neighboring tiles are 
+% then combined using bilinear interpolation to eliminate artificially induced boundaries. 
+% The contrast, especially in homogeneous areas, can be limited to avoid amplifying any 
+% noise that might be present in the image
+%
+% INPUT 
+%   1. input_args:  variable containing the analysis object
+%
+% OUTPUT
+%   1. status:  status elaboration (0  executed correctly; > 0 fatal error)
+%   2. argout:  variable containing a structure with output objects, description 
+%               and ref association
+%
+% REFERENCES
+%
+% AUTHOR:   Alexander Tournier (alexander.tournier@cancer.org.uk)
+%           Andreas Hoppe (A.Hoppe@kingston.ac.uk)
+%           Davide Martin Heller (davide.heller@imls.uzh.ch)
+%           Lorenzo Gatti (lorenzo.gatti@alumni.ethz.ch)
+%
+% DATE:     2.09.14 V0.1 for EpiTools 0.1 beta
+%           5.12.14 V0.2 for EpiTools 2.0 beta
+% 
+% LICENCE:
+% License to use and modify this code is granted freely without warranty to all, as long as the 
+% original author is referenced and attributed as such. The original author maintains the right 
+% to be solely associated with this work.
 
+% Copyright by A.Tournier, A. Hoppe, D. Heller, L.Gatti
+% ------------------------------------------------------------------------------
+%% Retrieve supplementary arguments
+if (nargin<2)
+    varargin(1) = {'OUT1'};
+end
+%% Procedure initialization
+status = 1;
+% Tracking time of the computation
 tic
-
-tmpStgObj = stgObj.analysis_modules.Contrast_Enhancement.settings;
-%load(DataSpecificsPath);
-
-tmpRegObj = load([stgObj.data_analysisindir,'/RegIm']);
-        
+% Remapping parameter settings object
+tmpStgObj = input_args.analysis_modules.Contrast_Enhancement.settings;
+tmpRegObj = load([input_args.data_analysisindir,'/RegIm']);
+% Display informations about the current module        
 % -------------------------------------------------------------------------
 log2dev('*********************** CLAHE MODULE **********************','INFO');
 log2dev('* Authors: A.Tournier, A. Hoppe, D. Heller, L.Gatti       * ','INFO');
@@ -17,24 +54,22 @@ log2dev('* Revision: 0.1 beta    $ Date: 2014/09/02 11:37:00       *','INFO');
 log2dev('***********************************************************','INFO');  
 log2dev('Started clahe analysis module', 'INFO');
 % -------------------------------------------------------------------------        
-        
+% Display informations about the elaborations      
 progressbar('Enhancing contrast...(please wait)');
-
-%% assuming that images are either 8 or 16bit in input
+%% Check for correct formats
+% Assuming that images are either 8 or 16bit in input
 if ~isa(tmpRegObj.RegIm, 'uint16') && ~isa(tmpRegObj.RegIm, 'uint8')
-    error('Images should have either 8 bit or 16 bit pixel depth');
+    log2dev('Images should have either 8 bit or 16 bit pixel depth','ERR');
+    argout = struct();
+    return;
 end
-
-%pre-allocate output
+% Pre-allocate output
 RegIm_clahe = zeros(size(tmpRegObj.RegIm), class(tmpRegObj.RegIm));
-
-
 %% Apply CLAHE
 for i=1:size(tmpRegObj.RegIm,3)
     %parameter needs to be adapted for specific image input:
-    
     RegIm_uint = tmpRegObj.RegIm(:,:,i);
-    
+
     sizeX = size(tmpRegObj.RegIm,1);
     sizeY = size(tmpRegObj.RegIm,2);
     
@@ -63,8 +98,8 @@ log2dev(sprintf('Finished after %.2f', elapsedTime), 'DEBUG');
 % -------------------------------------------------------------------------
 progressbar(1);
 %% Inspect results
-if(~stgObj.exec_commandline)
-    if(stgObj.icy_is_used)
+if(~input_args.exec_commandline)
+    if(input_args.icy_is_used)
         icy_vidshow(RegIm_clahe,'CLAHE Sequence');
         
         % -------------------------------------------------------------------------
@@ -74,7 +109,7 @@ if(~stgObj.exec_commandline)
 
         
     else
-        if(strcmp(stgObj.data_analysisindir,stgObj.data_analysisoutdir))
+        if(strcmp(input_args.data_analysisindir,input_args.data_analysisoutdir))
             
             fig = getappdata(0  , 'hMainGui');
             handles = guidata(fig);
@@ -157,7 +192,7 @@ if(~stgObj.exec_commandline)
             
             
         else
-            firstrun = load([stgObj.data_analysisindir,'/RegIm']);
+            firstrun = load([input_args.data_analysisindir,'/RegIm']);
             % The program is being executed in comparative mode
             StackView(firstrun.RegIm,'hMainGui','figureC1');
             StackView(RegIm_clahe,'hMainGui','figureC2');
@@ -175,35 +210,42 @@ else
     StackView(RegIm_clahe);
     saveClahe();
 end
+%% Output formatting
+% Each single output need to be described in order to be used for variable exportation.
+% ARGOUT variable is a structure object
+% argout(1...).description = char();
+% argout(1...).ref = variable reference;
+% argout(1...).object = undefined;
+% First output variable
+argout(1).description = 'File path';
+argout(1).ref = varargin(1);
+%% Status execution update 
+status = 0;
 end
+
 %% Callback functions
 function out = ctrlAcceptResult_callback(hObject,eventdata,handles)
-
     out = 'Accept result';
     %backup previous result
     saveClahe();
     uiresume(fig);
 
     StackView(RegIm_clahe,'hMainGui','figureA');
-
 end
 % -------------------------------------------------------------------------
 function out = ctrlDiscardResult_callback(hObject,eventdata,handles)
-
     out = 'Discard result';
     setappdata(fig,'uidiag_userchoice', out);
     uiresume(fig);
 
     StackView(tmpRegObj.RegIm,'hMainGui','figureA');
-
 end
 % -------------------------------------------------------------------------
 function saveClahe()
-
     %save new version with contrast enhancement
     RegIm = RegIm_clahe;
     stgObj.AddResult('Contrast_Enhancement','clahe_path','RegIm_wClahe.mat');
     save([stgObj.data_analysisoutdir,'/RegIm_wClahe'],'RegIm');
-
+    argout(1).object = [stgObj.data_analysisoutdir,'/RegIm_wClahe'];
 end
 % -------------------------------------------------------------------------
