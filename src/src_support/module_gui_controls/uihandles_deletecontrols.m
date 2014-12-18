98cfbfef6ebe9f85fa2ec5e2ed76ec2553824874
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2014-12-18 17:54:18 +0100

    ECS-122 #ADD: Added integration for client-server framework for legacy module DELETE HANDLE CONTROLS. Added caller, function, header and tag description.

--- src/src_support/module_gui_controls/uihandles_deletecontrols.m
+++ src/src_support/module_gui_controls/uihandles_deletecontrols.m
@@ -11,25 +11,54 @@ switch input
     case 'all'
         items = fields(hUIControls);
         if(~isempty(items))
-        for i=1:numel(items)
-            try
-            delete(hUIControls.(char(items(i))));
-            catch
+            for i=1:numel(items)
+                if isa(hUIControls.(char(items(i))),'struct')
+                    for o=1:numel(hUIControls.(char(items(i))))
+                            
+                            delete(hUIControls.(char(items(i)))(o).panel);
+
+                    end
+                else
+                    try
+                        delete(hUIControls.(char(items(i))));
+                    catch err
+                        log2dev(sprintf('EPITOOLS:uihandles_deletecontrols:RemovingHandlesNotSuccesfull | %s',...
+                                        err.message),...
+                                'ERR');
+
+                    end
+                end
+                hUIControls = rmfield(hUIControls,char(items(i)));
             end
-            hUIControls = rmfield(hUIControls,char(items(i)));
         end
-        end
-    otherwise
-        
+    otherwise       
         if (isfield(hUIControls, input))
+            if isa(hUIControls.(char(input)),'struct')
+                for i=1:numel(hUIControls.(char(input)))
+                    if isempty(fields(hUIControls.(char(input))))
+                        hUIControls = rmfield(hUIControls,char(input));
+                        setappdata(hObject,'hUIControls',hUIControls);
+                    else
+                        delete(hUIControls.(char(input))(i).panel); 
+                    end
+                end
+            else
+                try
+                    delete(hUIControls.(char(input)));
+                catch err
+                    log2dev(sprintf('EPITOOLS:uihandles_deletecontrols:RemovingHandlesNotSuccesfull | %s',...
+                                                        err.message),...
+                                               'ERR');
+                end 
+            end 
             try
-            delete(hUIControls.(char(input)));
-            catch
+                hUIControls = rmfield(hUIControls,char(input)); 
+            catch err
+                    log2dev(sprintf('EPITOOLS:uihandles_deletecontrols:RemovingHandlesNotSuccesfull | %s',...
+                                                        err.message),...
+                                               'ERR');
             end
-            hUIControls = rmfield(hUIControls,char(input));
-            
         end
-        
         
 end
         
