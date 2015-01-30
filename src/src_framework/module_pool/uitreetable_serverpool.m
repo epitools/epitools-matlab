b9e3bf4c18285ae21e42185fd5771832a53efc3b
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2015-01-30 02:18:11 +0100

    # REFACTORING: Refactoring treetable server pool code. Polishing from commented code

--- src/src_framework/module_pool/uitreetable_serverpool.m
+++ src/src_framework/module_pool/uitreetable_serverpool.m
@@ -1,11 +1,9 @@
 function jtable = uitreetable_serverpool(obj, globalHandle)
 %Uitreetable_serverqueue Summary of this function goes here
-
 headers = {'Pool','Label', 'Active'};
 rawdata = {};
 %% Data preparation
 % for each pool in the ref pool obj
-
 for i = 1:size(globalHandle(2:end),2)
     name = globalHandle(i+1).ref.file;
     status = globalHandle(i+1).ref.active;
@@ -19,8 +17,6 @@ for i = 1:size(globalHandle(2:end),2)
 
 end
 if(size(rawdata,2) == 0); rawdata = cell(1,2); end
-
-
 %% Generate treetable
 % selector = {'One','Two','Many'};
 colTypes = {'label','label','label'};
@@ -29,41 +25,11 @@ icons = {fullfile('./images/icons/bookmark.png'), ...
          fullfile('./images/icons/book_open.png'), ...
          fullfile('./images/icons/book.png'), ...
 };
- 
 % Create the table in the current figure
 jtable = treeTable('Container',obj, 'Headers',headers, 'Data',rawdata, ...
                    'ColumnTypes',colTypes, 'ColumnEditable',colEditable, ...
                    'IconFilenames',icons, 'Groupable',true, 'InteractiveGrouping',false);
 
+%set(jtable,'MousePressedCallback', {@selectionCallback})
 %set(handle(jtable.getSelectionModel,'CallbackProperties'), 'ValueChangedCallback', {@selectionCallback,    jtable});
-
-
-%uitreetable_contextualmenu(jtable);
-
 end
-
-% %function selectionCallback(hSelectionModel,hEvent,jtable)
-%     % Get the selection data
-%     %AnchorSelectionIndex = get(hSelectionModel,'AnchorSelectionIndex');
-%     MinSelectionIndex  = get(hSelectionModel,'MinSelectionIndex');
-% 
-%     if (get(jtable.getRowAt(MinSelectionIndex), 'Level')==0)
-%         %root = MinSelectionIndex+1;
-%     else
-%         root = jtable.getRowAt(MinSelectionIndex).getParent.getLevel +1;
-%         hMainGui = getappdata(0, 'hMainGui');
-%         pool_instances = getappdata(hMainGui, 'pool_instances');
-%         if(pool_instances(root+1).ref.active)
-%             pool_instances(root+1).ref.deactivatePool;
-%         elseif(~pool_instances(root+1).ref.active)
-%             pool_instances(root+1).ref.activatePool;
-%         end
-%         
-%         pool_instances(root+1).ref.buildGUInterface(pool_instances(root+1).ref.handleGraphics, ...
-%             pool_instances);
-%         setappdata(hMainGui, 'pool_instances',pool_instances);
-%     end
-%     
-%     % Now do something useful with this info
-%     fprintf('Selected rows %d with parent %d\n', MinSelectionIndex, root);
-% end
