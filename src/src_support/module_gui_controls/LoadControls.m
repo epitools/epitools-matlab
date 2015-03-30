fc6a2a54772a2ad8c0b24829d06afb049aa9c759
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2015-03-30 15:36:38 +0200

    #FIX: Fixed minors in the load controls function. It might be deprecated.

--- src/src_support/module_gui_controls/LoadControls.m
+++ src/src_support/module_gui_controls/LoadControls.m
@@ -2,24 +2,32 @@ function LoadControls(obj,objSettings)
 %LOADCONTROLS Summary of this function goes here
 %   Detailed explanation goes here
 hMainGui = getappdata(0, 'hMainGui');
+
 uihandles_deletecontrols('uitree');
+
 %uihandles_deletecontrols('uipanel_serverqueue');
+% Create panel 
+uihandles_deletecontrols('uisidebarpanel');
+uisidebarpanel = uipanel('Parent', hMainGui,...
+        'Position',[0.0 0.00 0.17 1],...
+        'Units', 'normalized');
+uihandles_savecontrols( 'uisidebarpanel', uisidebarpanel);
 
 % Load JTREE Class
-jtree = uitree_control(obj,objSettings);
+jtree = uitree_control(uisidebarpanel,objSettings);
 
 % Load Contextual menu on JTREE class
 uitree_contextualmenu(jtree);
 
 if ~uihandles_exists('uipanel_serverqueue')
-    uipanel_serverqueue = uipanel('Parent', hMainGui,...
+    uipanel_serverqueue = uipanel('Parent', uisidebarpanel,...
                                   'Position',[0.0 0.02 0.17 0.20],...
                                   'Units', 'normalized');
     uihandles_savecontrols( 'uipanel_serverqueue', uipanel_serverqueue );
 end
 
 if ~uihandles_exists('uipanel_serverpool')
-    uipanel_serverpool = uipanel('Parent', hMainGui,...
+    uipanel_serverpool = uipanel('Parent', uisidebarpanel,...
                                   'Position',[0.0 0.22 0.17 0.10],...
                                   'Units', 'normalized');
     uihandles_savecontrols('uipanel_serverpool', uipanel_serverpool );
